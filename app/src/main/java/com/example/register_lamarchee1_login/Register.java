c94e6e5bfc77fb0d3684d208dd24cd40fea845d1
Eric Lamarche <ericelamarche@gmail.com>
2022-06-15 02:58:48 -0700

    Initial commit

---
+++ app/src/main/java/com/example/register_lamarchee1_login/Register.java
@@ -0,0 +1,69 @@
+package com.example.register_lamarchee1_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Register extends AppCompatActivity {
+
+    EditText edtFirstName, edtLastName, edtBirthday, edtPassword, etdEmail;
+    Button btnCancel, btnSubmit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        btnCancel = findViewById(R.id.btnRegisterCancel);
+        btnSubmit = findViewById(R.id.btnRegisterSubmit);
+        edtFirstName = findViewById(R.id.edtFirstName);
+        edtLastName = findViewById(R.id.edtLastName);
+        edtBirthday = findViewById(R.id.edtBirthday);
+        edtPassword = findViewById(R.id.edtPassword);
+        etdEmail = findViewById(R.id.edtEmail);
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //send back page was cancelled
+                Intent result = new Intent();
+                setResult(RESULT_CANCELED, result);
+                finish();
+
+            }
+        });
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String firstname = edtFirstName.getText().toString();
+                String lastname = edtLastName.getText().toString();
+                String birthday = edtBirthday.getText().toString();
+                String password = edtPassword.getText().toString();
+                String email = etdEmail.getText().toString();
+
+                //check to see if any info fields are blank
+                if (firstname.length() == 0 || lastname.length() == 0 || email.length() == 0 || password.length() == 0 || birthday.length() == 0) {
+                    Toast.makeText(Register.this, "Error: Please fill in all fields", Toast.LENGTH_LONG).show();
+
+                } else { //checks name length more than 2 but less than 31 characters, checks email has @ and .edu or .com and doesnt have typo like ..com
+                    if (firstname.length() < 3 || firstname.length() > 30) {
+                        Toast.makeText(Register.this, "Error: first name must be 3-30 characters", Toast.LENGTH_LONG).show();
+                    } else if (email.contains("@") == false || (email.contains(".edu") == false && email.contains(".com") == false) || email.contains("..") == true) {
+                        Toast.makeText(Register.this, "Error: email must contain @ and .com or .edu", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        //send back register successful
+                        Intent result = new Intent();
+                        setResult(RESULT_OK, result);
+                        finish();
+                    }
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/register_lamarchee1_login/SplashScreen.java
@@ -0,0 +1,33 @@
+package com.example.register_lamarchee1_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    TextView txtAppName;
+    int SHOW_TIME = 3000; //time to show splash screen in ms
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        txtAppName = findViewById(R.id.txtAppName);
+
+        //call finish 3 seconds from now
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, SHOW_TIME);
+
+    }
+}
